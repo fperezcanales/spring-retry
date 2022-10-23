4501c12f7d42ad7534974cdc685a2d4d181e9752
fperez <menucloud.cl@gmail.com>
2022-10-23 14:51:31 -0300

    Adds Spring Retry, Context, AOP, Aspect dependencies

--- src/main/java/com/example/myspringretry/MySpringRetryApplication.java
+++ src/main/java/com/example/myspringretry/MySpringRetryApplication.java
@@ -2,6 +2,9 @@ package com.example.myspringretry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
+@EnableRetry
 public class MySpringRetryApplication {
 
 	public static void main(String[] args) {
@@ -16,7 +20,15 @@ public class MySpringRetryApplication {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/")
+	@Retryable(value = { NumberFormatException.class }, maxAttempts = 5)
 	public String createOrder() {
+		Integer.parseInt("");
 		return "Success";
+	}
+
+	@Recover
+	public String recover(NumberFormatException ex){
+		System.out.println("Recover method for NumberFormatException");
+		return "Recover method for NumberFormatException";
 	}
 }
