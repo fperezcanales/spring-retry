55fa51e80c6a68be6288307fd7c6ef22404579ea
fperez <menucloud.cl@gmail.com>
2022-10-23 12:37:35 -0300

    Define OrderController to try Spring Retry

--- src/main/java/com/example/myspringretry/MySpringRetryApplication.java
+++ src/main/java/com/example/myspringretry/MySpringRetryApplication.java
@@ -2,12 +2,21 @@ package com.example.myspringretry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class MySpringRetryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringRetryApplication.class, args);
 	}
 
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String createOrder() {
+		return "Success";
+	}
 }
