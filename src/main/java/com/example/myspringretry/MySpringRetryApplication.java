e5a77c79c7395cef481f9c0d6c2b5839bc6c7760
fperez <menucloud.cl@gmail.com>
2022-10-23 11:44:40 -0300

    Adds Spring-web project as base

---
+++ src/main/java/com/example/myspringretry/MySpringRetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringRetryApplication.class, args);
+	}
+
+}
